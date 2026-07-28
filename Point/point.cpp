f71c55ae3c124513ba3caa46fd5a2ba41be5d102
josef-abricio <fabriciojosettp@gmail.com>
2026-07-27 21:22:31 -0300

    Aplicando colisao das janelas

--- Point/point.cpp
+++ Point/point.cpp
@@ -9,6 +9,23 @@ void Point::update(float gravity, float friction){
 
     pv.position.x += velocty_x;
     pv.position.y += velocty_y + gravity;
+
+    if( pv.position.x < 0){
+        pv.position.x = 0;
+        pv.old_position.x = pv.position.x + velocty_x;
+    }
+    if( pv.position.x > 800){
+        pv.position.x = 800;
+        pv.old_position.x = pv.position.x + velocty_x;
+    }
+    if( pv.position.y < 0){
+        pv.position.y = 0;
+        pv.old_position.y = pv.position.y + velocty_y;
+    }
+    if( pv.position.y > 600){
+        pv.position.y = 600;
+        pv.old_position.y = pv.position.y + velocty_y;
+    }
 }
 
 void Point::Draw(){
